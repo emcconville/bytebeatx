b793a18584e50e45e62dc560efca790185003c45
Eric McConville <emcconville@emcconville.com>
2016-02-09 20:45:43 -0600

    Added more beats

--- ByteBeatCLI/beats.c
+++ ByteBeatCLI/beats.c
@@ -1,6 +1,7 @@
 
 /* Standard */
 #include "string.h"
+#include "math.h"
 /* Me */
 #include "beats.h"
 
@@ -105,12 +106,12 @@ int8_t ccb(int t)
 
 int8_t ccc(int t)
 {
-    return (t*5&(t>>7))|(t*3&(t*4>>10));
+    return (t*5&(t>>7))|(t*3&(t*4>>10)); // mirro
 }
 
 int8_t ccd(int t)
 {
-    return (t>>7|t|t>>6)*10+4*((t&t>>13)|t>>6);
+    return (t>>7|t|t>>6)*10+4*((t&t>>13)|t>>6); // viznut
 }
 
 int8_t cce(int t)
@@ -145,6 +146,77 @@ int8_t eed(int t)
     return t*(3&t>>11)+(t&t>>11)*4*!((t>>11)%3);
 }
 
+int8_t oxa(int t)
+{
+    return ((t>>1%128)+20)*3*t>>14*t>>18; // harism
+}
+
+int8_t oxb(int t)
+{
+    return t*(((t>>9)&10)|((t>>11)&24)^((t>>10)&15&(t>>15))); // tangent128
+}
+
+int8_t oxc(int t)
+{
+    return (t*t/256)&(t>>((t/1024)%16))^t%64*(0xC0D3DE4D69>>(t>>9&30)&t%32)*t>>18; // ultrageranium
+}
+
+int8_t shs1(int t)
+{
+    return (t|t>>8)>>(t>>7)|(t<<6|t<<8)>>(t>>11)|(t<<7|t<<11)>>(t>>11); // hadez
+}
+int8_t shs2(int t)
+{
+    return (t|t>>17)>>(t>>7)|(t<<2|t<<8)>>(t>>11)|(t<<2|t<<11)>>(t>>17); // h0uz3
+}
+int8_t shs3(int t)
+{
+    return (int8_t)sin(t>>16)|(t|t>>8); // hadez
+}
+int8_t shs4(int t)
+{
+    return (t>>9&t>>8)*(t>>3); // h0uz3
+}
+int8_t shs5(int t)
+{
+    return (t>>8|t-2)&(2-t|8<<t); // h0uz3
+}
+int8_t shs6(int t)
+{
+    return ((t>>8|t-2)&(2-t|8<<t))<<((t>>8|t-2)&(2-t|8<<t))>>((t>>8|t-2)&(2-t|8<<t)); // h0uz3
+}
+int8_t shs7(int t)
+{
+    return ((t>>3|t<<7)|t>>11)|t>>16; // h0uz3
+}
+int8_t shs8(int t)
+{
+    return ((((t>>3|t<<7)|t>>11)|t>>16)&23); // h0uz3
+}
+int8_t shs9(int t)
+{
+// FIXME!
+//    return ((sin((t%17)*3.1415*0.5)*100)<<t/90)|(sin(t*t*3.1415*0.5)*50); // samuirai
+    return 0;
+}
+int8_t shs10(int t)
+{
+    return ((t>>4)|((t<<4)&0x11111+t/1000)); // tv
+}
+int8_t shs11(int t)
+{
+    return t<700000?((t/2000%2?t<<3:t<<2)>>(t/10000%3)<<t/100000):t<800000?-t*t*t>>(t/1000)|t/(800000-t):t<805000?t<<(t/500):/*"fin"*/1/(t-t); // tv
+}
+int8_t shs12(int t)
+{
+    return ((t>>7)* 0xff) * (t & 0xff)/32 & ~(t / 127); // bdwheele
+}
+int8_t shs13(int t)
+{
+    int l[4] = {8,9,10,11};
+    return ((t>>10)<<((t>>4)%(l[(t>>11)%4]-(t>>15)%4))); // tito
+}
+
 static audio_beat_t catalog[50] = {
     {"aaa", "viznut", aaa},
     {"aab",    "bst", aab},
@@ -163,14 +235,30 @@ static audio_beat_t catalog[50] = {
     {"bbg", "viznut", bbg},
     {"cca", "viznut", cca},
     {"ccb",   "red-", ccb},
-    {"ccc",       "", ccc},
-    {"ccd",       "", ccd},
+    {"ccc",  "miiro", ccc},
+    {"ccd", "viznut", ccd},
     {"cce",  "skurk", cce},
     {"ccf","xpensive",ccf},
-    {"eea",  "Jeff Burdges", eea},
+    {"eea","Jeff Burdges", eea},
     {"eeb",  "JiminP", eeb},
     {"eec",  "JiminP", eec},
     {"eed",  "JiminP", eed},
+    {"oxa",  "herism", oxa},
+    {"oxb","tangent128", oxb},
+    {"oxc","ultrageranium", oxc},
+    {"shs1",  "hadez",shs1},
+    {"shs2",  "h0uz3",shs2},
+    {"shs3",  "hadez",shs3},
+    {"shs4",  "h0uz3",shs4},
+    {"shs5",  "h0uz3",shs5},
+    {"shs6",  "h0uz3",shs6},
+    {"shs7",  "h0uz3",shs7},
+    {"shs8",  "h0uz3",shs8},
+    {"shs9","samuirai",shs9},
+    {"shs10",    "tv",shs10},
+    {"shs11",    "tv",shs11},
+    {"shs12","bdwheele",shs12},
+    {"shs13",  "tito",shs13},
     { NULL,     NULL, NULL}
 };
 
